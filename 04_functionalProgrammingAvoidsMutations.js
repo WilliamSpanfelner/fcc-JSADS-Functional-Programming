1c2b1605cbc3a85e0ce614e78b80281a2473c9ad
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-09 20:53:30 +0300

    Challenge - start

--- 04_functionalProgrammingAvoidsMutations.js
+++ 04_functionalProgrammingAvoidsMutations.js
@@ -3,3 +3,15 @@
 /* Changing or altering values is called mutation in functional
 programming, while the outcome is called a side-effect.  Functions
 should be pure functions that do not cause side effects. */
+
+/* Fill in the code for the function incrementer so it returns the 
+value of the global variable fixedValue increased by one. */
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+
+
+  // Only change code above this line
+}
