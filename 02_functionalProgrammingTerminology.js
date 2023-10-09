b32a126a457f1fc2df6fae13cc752672bad39a1c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-09 13:42:16 +0300

    Define callbacks, 1st class functions, higher order functions and lambda functions.

--- 02_functionalProgrammingTerminology.js
+++ 02_functionalProgrammingTerminology.js
@@ -1,1 +1,17 @@
 // Understand Functional Programming Terminology
+
+/* 
+Callbacks are functions that are passed as arguments
+to another function to decide how that function will be 
+invoked. 
+
+First class functions (all functions in JavaScript), can
+be returned from a function, passed into another function or
+assigned to a variable like any other value.
+
+Higher order functions can take a function as an argument
+or return a function.
+
+A lambda function can take a function as an argument or
+return a function.
+*/ 
