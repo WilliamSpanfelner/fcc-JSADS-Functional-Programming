b4c08385846cc5204a85064992a1627ee80a5e34
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-09 11:25:34 +0300

    Add some characteristics of Functional Programming

--- 01_aboutFunctionalProgramming.js
+++ 01_aboutFunctionalProgramming.js
@@ -1,1 +1,7 @@
 // Learn About Functional Programming
+
+/* Functional programming is about: 
+1. Isolated functions that don't depend upon the state or global variables;
+2. Pure functions that yeild the same output for the same input;
+3. Functions with limited side-effects.
+*/
