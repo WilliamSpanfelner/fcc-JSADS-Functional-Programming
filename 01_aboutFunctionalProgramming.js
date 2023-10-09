f3f5c0008e113265d7d6d1f176a2c59e093798c8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-09 11:32:52 +0300

    Challenge - start

--- 01_aboutFunctionalProgramming.js
+++ 01_aboutFunctionalProgramming.js
@@ -5,3 +5,10 @@
 2. Pure functions that yeild the same output for the same input;
 3. Functions with limited side-effects.
 */
+
+/* The members of freeCodeCamp happen to love tea.
+
+In the code editor, the prepareTea and getTea functions are 
+already defined for you. Call the getTea function to get 40 
+cups of tea for the team, and store them in the tea4TeamFCC 
+variable. */
