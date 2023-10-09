831dd7410a2feda90bb03f6d42da110e59796541
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-09 20:51:43 +0300

    Add characteristics of mutations

--- 04_functionalProgrammingAvoidsMutations.js
+++ 04_functionalProgrammingAvoidsMutations.js
@@ -1,1 +1,5 @@
 // Avoid Mutations and Side Effects Using Functional Programming
+
+/* Changing or altering values is called mutation in functional
+programming, while the outcome is called a side-effect.  Functions
+should be pure functions that do not cause side effects. */
