dfa71709234ee2385da5c6a3df6c3bedbd251709
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-09 21:03:28 +0300

    Create some output

--- 04_functionalProgrammingAvoidsMutations.js
+++ 04_functionalProgrammingAvoidsMutations.js
@@ -11,7 +11,10 @@ let fixedValue = 4;
 
 function incrementer() {
   // Only change code below this line
-
+    return fixedValue + 1;
 
   // Only change code above this line
 }
+
+const newValue = incrementer();
+console.log("fixedValue: " + fixedValue + "\nnewValue: " + newValue);
