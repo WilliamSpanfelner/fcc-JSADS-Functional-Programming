a3a270d7d58aaba8648c860876f685f9d1995da1
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-10 15:27:30 +0300

    Illustrate map function

--- 07_mapToExtractArrayData.js
+++ 07_mapToExtractArrayData.js
@@ -1,1 +1,21 @@
 // Use the map Method to Extract Data from an Array
+/* Functions can be stored in variables, objects or
+passed as function arguments */
+
+/* The Array.prototype.map() or just map method iterates
+over an array and returns a new array containing the elements
+amended or not by a callback function.  The original array
+is not mutated.
+
+The callback function uses three arguments: the current element;
+the index of the current element; and the array on which the 
+callback function was called. */
+
+const users = [
+    {name: "John", age: 34},
+    {name: "Amy", age: 20},
+    {name: "camperCat", age: 10},    
+];
+
+const names = users.map(user => user.name);
+console.log(names);  // ['John', 'Amy', 'camperCat']
