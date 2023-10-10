d09e53e721d51691d22d744b986c506ac1c4aad8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-10 10:57:06 +0300

    Challenge - start

--- 05_avoidExternalDependencies.js
+++ 05_avoidExternalDependencies.js
@@ -2,3 +2,21 @@
 
 /* Declare dependencies explicity by passing a variable or 
 object directly to a function as an argument. */
+
+/*
+Let's update the incrementer function to clearly declare its 
+dependencies.
+
+Write the incrementer function so it takes an argument, and 
+then returns a result after increasing the value by one.
+*/
+
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer() {
+
+
+  // Only change code above this line
+}
