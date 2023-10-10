cf659cb51fa319da13456e0cd2f6b876ca41eafe
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-10 12:14:02 +0300

    Preserve integrity of variables & declare function parameters

--- 06_refactorGlobalsOutOfFunctions.js
+++ 06_refactorGlobalsOutOfFunctions.js
@@ -1,1 +1,6 @@
 // Refactor Global Variables Out of Functions
+
+/* Two principles to follow when using functional programming:
+1. Variables and objects should have their integrity preserved;
+2. Function parameters should be declared.
+*/
