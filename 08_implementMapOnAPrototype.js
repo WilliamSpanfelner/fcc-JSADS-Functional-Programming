a060e2b9b0d42700d730f24dceca8784059e7280
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-10 18:55:13 +0300

    Challenge - start

--- 08_implementMapOnAPrototype.js
+++ 08_implementMapOnAPrototype.js
@@ -1,1 +1,16 @@
 // Implement map on a Prototype
+
+/*
+Write your own Array.prototype.myMap(), which 
+should behave exactly like Array.prototype.map(). 
+You should not use the built-in map method. The 
+Array instance can be accessed in the myMap 
+method using this. 
+*/
+Array.prototype.myMap = function(callback) {
+    const newArray = [];
+    // Only change code below this line
+    
+    // Only change code above this line
+    return newArray;
+};
