7b3f4b3735f9404e5fec510144bdd615e482d9e6
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-12 13:22:19 +0300

    Challenge - solution

--- 08_implementMapOnAPrototype.js
+++ 08_implementMapOnAPrototype.js
@@ -10,7 +10,15 @@ method using this.
 Array.prototype.myMap = function(callback) {
     const newArray = [];
     // Only change code below this line
-    
+    console.log(this, callback);
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i], i, this));
+    }
+
     // Only change code above this line
     return newArray;
 };
+
+console.log([1,2,3,4,5].map((x) => x * 2));  // [2, 4, 6, 8, 10]
+console.log([1,2,3,4,5].myMap((x) => x ** 2));  // [1, 4, 9, 16, 25]
+console.log([1,1,2,5,2].myMap((element, index, array) => array[index + 1] || array[0]));
