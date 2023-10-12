6850da37902ca2363a5fa00d2ca1dc2a0a7c29fc
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-12 21:52:27 +0300

    Add test code to understand callback better

--- 08_implementMapOnAPrototype.js
+++ 08_implementMapOnAPrototype.js
@@ -22,3 +22,46 @@ Array.prototype.myMap = function(callback) {
 console.log([1,2,3,4,5].map((x) => x * 2));  // [2, 4, 6, 8, 10]
 console.log([1,2,3,4,5].myMap((x) => x ** 2));  // [1, 4, 9, 16, 25]
 console.log([1,1,2,5,2].myMap((element, index, array) => array[index + 1] || array[0]));
+
+console.log("\n\n this = ");
+
+function showContext() {
+    console.log(this);
+}
+showContext();  /* global {
+                        global: global,
+                        queueMicrotask: ƒ, 
+                        clearImmediate: ƒ, 
+                        setImmediate: ƒ, 
+                        structuredClone: ƒ, 
+                        …} */
+
+const objectA = {};
+objectA.showContext = showContext;
+objectA.showContext();  // {showContext: ƒ}
+
+const objectB = {};
+// objectB.showContext();  // TypeError: objectB.showContext is not a function
+
+Object.prototype.showContext = showContext;
+objectB.showContext();
+
+console.log("\n");
+const magic = () => new Date();
+console.log(magic());
+
+console.log("\n");
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1,2], [3,4,5]));
+
+console.log("\n");
+const increment = (number, value = 1) => number + value;
+console.log(increment(5));
+console.log(increment(5, 5));
+
+function logThis() {
+    "use strict";
+    console.log(this);
+}
+
+[1,2,3].forEach(logThis, {name: 'obj'});
