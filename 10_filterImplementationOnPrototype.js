00cd6cd5373a4ad7a6ca53b08ede4881a0d7bafc
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-13 12:00:32 +0300

    Challenge - solution

--- 10_filterImplementationOnPrototype.js
+++ 10_filterImplementationOnPrototype.js
@@ -9,10 +9,16 @@ can be accessed in the myFilter method using this.
 Array.prototype.myFilter = function(callback) {
     const newArray = [];
     // Only change code below this line
-    this.forEach((element, index, array) => (callback(element, index, array) ? newArray.push(element) : element));  // iterate through array & apply callback if truthy
+    console.log(this);
+    this.forEach((element, index, array) => 
+        (callback(element, index, array) ? 
+        newArray.push(element) : 
+        element));  // iterate through array & apply callback if truthy
     console.log(newArray);
     // Only change code above this line
     return newArray;
 };
 
-[23, 65, 98, 5, 13].myFilter(item => item % 2)  // [23, 65, 5, 13]
+// [23, 65, 98, 5, 13].myFilter(item => item % 2)  // [23, 65, 5, 13]
+// ["naomi", "quincy", "camperbot"].myFilter(element => element === "naomi")  // ['naomi']
+[1, 1, 2, 5, 2].myFilter((element, index, array) => array.indexOf(element) === index)  // [1, 2, 5]
