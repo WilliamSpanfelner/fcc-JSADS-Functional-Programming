604a7212b3ab4f3491507e2f3f7b4a9689deedf6
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-13 21:18:02 +0300

    Challenge - start

--- 12_useSliceToRemoveElements.js
+++ 12_useSliceToRemoveElements.js
@@ -9,3 +9,20 @@ the original array.*/
 const cities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
 cities.splice(3, 1);
 console.log(cities);  // ['Chicago', 'Delhi', 'Islamabad', 'Berlin']
+
+/* This constrasts with the slice method that returns
+a new array. */
+
+/*
+Rewrite the function nonMutatingSplice by using slice instead 
+of splice. It should limit the provided cities array to a 
+length of 3, and return a new array with only the first three 
+items.
+
+Do not mutate the original array provided to the function.
+*/
+
+function nonMutatingSplice(cities) {
+
+    return cities.splice(3);
+}
