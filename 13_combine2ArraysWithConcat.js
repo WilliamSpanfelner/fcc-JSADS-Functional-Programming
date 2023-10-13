019160af25a374549a229f0227f5a117435e1880
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-13 21:45:26 +0300

    Challenge - start

--- 13_combine2ArraysWithConcat.js
+++ 13_combine2ArraysWithConcat.js
@@ -3,3 +3,18 @@
 // The concat method combines strings or arrays
 
 console.log([1, 2, 3].concat([4, 5, 6]));  // [1, 2, 3, 4, 5, 6]
+
+/* Use the concat method in the nonMutatingConcat function to 
+concatenate attach to the end of original. The function should 
+return the concatenated array. */
+
+function nonMutatingConcat(original, attach) {
+    // Only change code below this line
+    
+  
+    // Only change code above this line
+}
+  
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingConcat(first, second);
