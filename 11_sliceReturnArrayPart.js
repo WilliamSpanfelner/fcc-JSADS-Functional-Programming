f77ea52f80daa5e6bea6cc788f37aeeb24e95803
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-13 16:15:02 +0300

    Challenge - start

--- 11_sliceReturnArrayPart.js
+++ 11_sliceReturnArrayPart.js
@@ -8,3 +8,17 @@ of array elements and can take two argument:
 const arr = ["Cat", "Dog", "Tiger", "Zebra"];
 const newArray = arr.slice(1,3);
 console.log(newArray);  // ['Dog', 'Tiger']
+
+/* Use the slice method in the sliceArray function to return part of 
+the anim array given the provided beginSlice and endSlice indices. 
+The function should return an array. */
+
+function sliceArray(anim, beginSlice, endSlice) {
+    // Only change code below this line
+  
+  
+    // Only change code above this line
+  }
+  
+  const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+  sliceArray(inputAnim, 1, 3);
