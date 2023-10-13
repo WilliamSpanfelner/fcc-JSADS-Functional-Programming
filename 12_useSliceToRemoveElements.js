ccd3c65b64b23414574555a8ed23fd33962f5106
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-13 21:11:38 +0300

    Add splice example

--- 12_useSliceToRemoveElements.js
+++ 12_useSliceToRemoveElements.js
@@ -1,1 +1,11 @@
 // Remove Elements from an Array Using slice Instead of splice
+
+/* The splice array method takes a start index and 
+the number of items to remove which defaults to the 
+end of the array if omitted.  An important 
+characteristic of splice, however, is that it mutates
+the original array.*/
+
+const cities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+cities.splice(3, 1);
+console.log(cities);  // ['Chicago', 'Delhi', 'Islamabad', 'Berlin']
