821fde9d8e45f1269f7a9e264e950eb320462099
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-13 21:48:23 +0300

    Challenge - solution

--- 13_combine2ArraysWithConcat.js
+++ 13_combine2ArraysWithConcat.js
@@ -10,11 +10,11 @@ return the concatenated array. */
 
 function nonMutatingConcat(original, attach) {
     // Only change code below this line
-    
+    return original.concat(attach);
   
     // Only change code above this line
 }
   
 const first = [1, 2, 3];
 const second = [4, 5];
-nonMutatingConcat(first, second);
+console.log(nonMutatingConcat(first, second));
