161e5518d8a4ff33cf9199058bee7e0bf7653a06
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-13 16:12:40 +0300

    Add slice example

--- 11_sliceReturnArrayPart.js
+++ 11_sliceReturnArrayPart.js
@@ -1,1 +1,10 @@
 // Return Part of an Array Using the slice Method
+/* The slice array method returns a copy of a group
+of array elements and can take two argument: 
+1. an index of where to begin the slice;
+2. an index of where to end the slice non-inclusively;
+*/
+
+const arr = ["Cat", "Dog", "Tiger", "Zebra"];
+const newArray = arr.slice(1,3);
+console.log(newArray);  // ['Dog', 'Tiger']
