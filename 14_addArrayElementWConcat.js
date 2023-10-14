e914f586dbeb29b67329652bade92c53055bc242
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-14 21:23:26 +0300

    Update indentation

--- 14_addArrayElementWConcat.js
+++ 14_addArrayElementWConcat.js
@@ -19,8 +19,8 @@ function nonMutatingPush(original, newItem) {
     return original.concat(newItem);
   
     // Only change code above this line
-  }
-  
-  const first = [1, 2, 3];
-  const second = [4, 5];
-  console.log(nonMutatingPush(first, second), first, second);
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+console.log(nonMutatingPush(first, second), first, second);
