d5212a2d6eb268a2f439312f1f8645c238805154
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-14 21:21:32 +0300

    Challenge - solution

--- 14_addArrayElementWConcat.js
+++ 14_addArrayElementWConcat.js
@@ -16,11 +16,11 @@ newItem arrays. The function should return an array.
 
 function nonMutatingPush(original, newItem) {
     // Only change code below this line
-    return original.push(newItem);
+    return original.concat(newItem);
   
     // Only change code above this line
   }
   
   const first = [1, 2, 3];
   const second = [4, 5];
-  nonMutatingPush(first, second);
+  console.log(nonMutatingPush(first, second), first, second);
