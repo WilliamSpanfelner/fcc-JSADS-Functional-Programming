9525d94848791b41a645cbb8c861c1731066cffd
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-14 21:11:14 +0300

    Add push example

--- 14_addArrayElementWConcat.js
+++ 14_addArrayElementWConcat.js
@@ -1,2 +1,9 @@
 /* Add Elements to the End of an Array Using 
 concat Instead of push */
+
+/* The push array method adds element to the end of an array,
+mutating the original array.  */
+
+const arr = [1, 2, 3];
+arr.push(4, 5, 6);
+console.log(arr);  // [1, 2, 3, 4, 5, 6]
