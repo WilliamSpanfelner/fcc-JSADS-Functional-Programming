13c4b0746944340dd64f271b0d631b8acb8aa6a0
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-14 21:13:20 +0300

    Challenge - start

--- 14_addArrayElementWConcat.js
+++ 14_addArrayElementWConcat.js
@@ -7,3 +7,20 @@ mutating the original array.  */
 const arr = [1, 2, 3];
 arr.push(4, 5, 6);
 console.log(arr);  // [1, 2, 3, 4, 5, 6]
+
+/*
+Change the nonMutatingPush function so it uses concat to merge 
+newItem to the end of original without mutating original or 
+newItem arrays. The function should return an array.
+*/
+
+function nonMutatingPush(original, newItem) {
+    // Only change code below this line
+    return original.push(newItem);
+  
+    // Only change code above this line
+  }
+  
+  const first = [1, 2, 3];
+  const second = [4, 5];
+  nonMutatingPush(first, second);
