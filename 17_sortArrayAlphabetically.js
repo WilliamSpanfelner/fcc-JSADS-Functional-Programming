0d8b18bd9e37ddbc242fdfb1cf6725bf2814bfeb
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-16 20:02:15 +0300

    Challenge - start

--- 17_sortArrayAlphabetically.js
+++ 17_sortArrayAlphabetically.js
@@ -15,3 +15,18 @@ function reverseAlpha(arr) {
 }
 
 console.log(reverseAlpha(['l', 'h', 'z', 'b', 's'])); // ['z', 's', 'l', 'h', 'b']
+
+/*
+Use the sort method in the alphabeticalOrder function to sort 
+the elements of arr in alphabetical order. The function should 
+return the sorted array. 
+*/
+
+function alphabeticalOrder(arr) {
+    // Only change code below this line
+  
+    return arr
+    // Only change code above this line
+}
+  
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
