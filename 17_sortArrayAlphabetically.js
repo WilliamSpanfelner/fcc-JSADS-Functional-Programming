83a9057ad4f5a94fe8cdd55e90f3fd93e5129043
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-16 20:00:32 +0300

    Add reverseAlpha sort example

--- 17_sortArrayAlphabetically.js
+++ 17_sortArrayAlphabetically.js
@@ -7,3 +7,11 @@ function ascendingOrder(arr) {
 }
 
 console.log(ascendingOrder([1, 5, 2, 3, 4]));  // [1, 2, 3, 4, 5]
+
+function reverseAlpha(arr) {
+    return arr.sort(function (a, b) {
+        return a === b ? 0 : a < b ? 1 : -1;
+    });
+}
+
+console.log(reverseAlpha(['l', 'h', 'z', 'b', 's'])); // ['z', 's', 'l', 'h', 'b']
