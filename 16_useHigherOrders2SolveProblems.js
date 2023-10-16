d42e3527379f03c05adb5fbd3a1fef332ee0ffb9
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-16 19:42:30 +0300

    Disallow negatives and square elements

--- 16_useHigherOrders2SolveProblems.js
+++ 16_useHigherOrders2SolveProblems.js
@@ -15,16 +15,16 @@ loops or the forEach() function.
 
 const squareList = arr => {
   // Only change code below this line
-  const arrIntegers = arr.filter((element) => Number.isInteger(element));
-  console.log(arrIntegers);
+  const arrIntegers = arr.filter((element) => Number.isInteger(element) && element > 0);
+  
+//   const newArray = arrIntegers.map((element) => element ** 2);
+//   console.log(arrIntegers, newArray);
 
-  return arr;
+  return arrIntegers.map((element) => element ** 2);
   // Only change code above this line
 };
 
-const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);  // [25, 9]
+// squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]) // [16, 1764, 36].
+// squareList([-3.7, -5, 3, 10, 12.5, 7, -4.5, -17, 0.3]) // [9, 100, 49].
 console.log(squaredIntegers);
-
-
-squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]) // [16, 1764, 36].
-squareList([-3.7, -5, 3, 10, 12.5, 7, -4.5, -17, 0.3]) // [9, 100, 49].
