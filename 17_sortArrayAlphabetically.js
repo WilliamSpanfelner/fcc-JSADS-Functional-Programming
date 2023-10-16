439397f64122d91d3cf6856ba94aeb4a329ed8e7
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-16 19:52:47 +0300

    Sort a numeric array ascending

--- 17_sortArrayAlphabetically.js
+++ 17_sortArrayAlphabetically.js
@@ -1,1 +1,9 @@
 // Sort an Array Alphabetically using the sort Method
+
+function ascendingOrder(arr) {
+    return arr.sort(function (a, b) {
+        return a - b;
+    });
+}
+
+console.log(ascendingOrder([1, 5, 2, 3, 4]));  // [1, 2, 3, 4, 5]
