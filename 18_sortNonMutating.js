5c587d2f243d434bd0a4b25b7803c82734e3b88b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-17 11:53:19 +0300

    Add non-mutating sort example

--- 18_sortNonMutating.js
+++ 18_sortNonMutating.js
@@ -1,3 +1,20 @@
 /* Return a Sorted Array Without Changing 
 the Original Array */
 
+/* The sort method mutates the original array in terms of the 
+order of the array elements. 
+
+A way to avoid this is to first concatenate an empty array to 
+the one being sorted (remembering that slice and concat return
+a new array), then run the sort method.
+*/
+
+function alphabeticalOrder(arr) {
+    const newArray = arr.concat([]);
+    console.log(arr);
+    return newArray.sort(function (a, b) {
+        return a === b ? 0 : a > b ? 1 : -1;
+    });
+}
+  
+console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
