0d11a086d8f1960815620880fe810825725d431b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-17 14:54:30 +0300

    Challenge - solution - Split then join string

--- 20_joinArrayWithString.js
+++ 20_joinArrayWithString.js
@@ -17,8 +17,8 @@ For this challenge, do not use the replace method.
 function sentensify(str) {
     // Only change code below this line
   
-  
+    return str.split(/\W/).join(" ");
     // Only change code above this line
 }
 
-sentensify("May-the-force-be-with-you");
+console.log(sentensify("May-the-force-be-with-you"));
