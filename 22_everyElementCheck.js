0fd59d94969b1374dfb132d62d85734f18e58099
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-17 18:11:29 +0300

    Add every method example

--- 22_everyElementCheck.js
+++ 22_everyElementCheck.js
@@ -1,2 +1,11 @@
 /* Use the every Method to Check that Every 
 Element in an Array Meets a Criteria */
+
+/* The every method tests each element of
+an array to return a single boolean result */
+
+const numbers = [1, 5, 8, 0, 10, 11];
+
+console.log(numbers.every(function (currentValue) {
+    return currentValue < 10;
+}));  // false
