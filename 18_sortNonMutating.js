b362f0621ca4fcd71f975eb08278501eb7012bd1
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-17 12:01:29 +0300

    Concatenate empty array to original and sort

--- 18_sortNonMutating.js
+++ 18_sortNonMutating.js
@@ -30,8 +30,12 @@ const globalArray = [5, 6, 3, 2, 9];
 function nonMutatingSort(arr) {
   // Only change code below this line
 
-  
+  const newArray = arr.concat([]);
+  return newArray.sort(function (a, b) {
+      return a === b ? 0 : a > b ? 1 : -1;
+  });
+
   // Only change code above this line
 }
 
-nonMutatingSort(globalArray);
+console.log(nonMutatingSort(globalArray));
