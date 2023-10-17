e8ac249582262eb371121674899d5a1f08bceb11
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-17 18:36:52 +0300

    Challenge - solution

--- 22_everyElementCheck.js
+++ 22_everyElementCheck.js
@@ -9,3 +9,18 @@ const numbers = [1, 5, 8, 0, 10, 11];
 console.log(numbers.every(function (currentValue) {
     return currentValue < 10;
 }));  // false
+
+/*
+Use the every method inside the checkPositive function to 
+check if every element in arr is positive. The function 
+should return a Boolean value.
+*/
+
+function checkPositive(arr) {
+    // Only change code below this line
+    return arr.every((currentValue) => currentValue >= 0);
+    // Only change code above this line
+}
+
+// console.log(checkPositive([1, 2, 3, -4, 5]));  // false
+console.log(checkPositive([1, 2, 3, 4, 5]));  // 
