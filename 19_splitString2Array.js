dcdb4f20cebdf8d338e2c7c7b5381868ef438b33
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-17 13:26:31 +0300

    Challenge - start

--- 19_splitString2Array.js
+++ 19_splitString2Array.js
@@ -14,3 +14,18 @@ console.log(byDigits);  // ['How', 'are', 'you', 'today']
 
 /* Splitting strings can make it easier to work with them 
 especially since strings are immutable. */
+
+/*
+Use the split method inside the splitify function to split str 
+into an array of words. The function should return the array. 
+Note that the words are not always separated by spaces, and the 
+array should not contain punctuation.
+*/
+function splitify(str) {
+    // Only change code below this line
+  
+  
+    // Only change code above this line
+  }
+  
+  splitify("Hello World,I-am code");
