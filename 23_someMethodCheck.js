7a29463267c3f23185c4178e42430049e648b4dc
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-17 18:42:41 +0300

    Add some method example

--- 23_someMethodCheck.js
+++ 23_someMethodCheck.js
@@ -1,2 +1,12 @@
 /* Use the some Method to Check that Any 
 Elements in an Array Meet a Criteria */
+
+/* Similar to the every method is the some method.
+It checks if at least one element will pass a test
+and returns a boolean. */
+
+const numbers = [10, 50, 8, 220, 110, 11];
+
+console.log(numbers.some(function (currentValue) {
+    return currentValue < 10;
+}));
