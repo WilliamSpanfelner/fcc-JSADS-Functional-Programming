f5f6c18c2892517476836b6ff52dd80f6cc86731
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-17 13:25:08 +0300

    Add examples of splitting string by space and regex

--- 19_splitString2Array.js
+++ 19_splitString2Array.js
@@ -1,1 +1,16 @@
 // Split a String into an Array Using the split Method
+
+/* The split method take an argument for a delimiter, 
+that can either be a character or a regular expression. 
+*/
+
+const str = "Hello World";
+const bySpace = str.split(" ");
+console.log(bySpace);  // ['Hello', 'World']
+
+const otherString = "How9are7you2today";
+const byDigits = otherString.split(/\d/);
+console.log(byDigits);  // ['How', 'are', 'you', 'today']
+
+/* Splitting strings can make it easier to work with them 
+especially since strings are immutable. */
