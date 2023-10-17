a550be8a3938768c3c7f3ed3e7c023ee85c12591
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-17 14:48:16 +0300

    Add example using join method.

--- 20_joinArrayWithString.js
+++ 20_joinArrayWithString.js
@@ -1,1 +1,8 @@
 // Combine an Array into a String Using the join Method
+
+/* Joining the elements of an array to create a string is
+done with the join method. */
+
+const arr = ["Hello", "World"];
+const str = arr.join(" ");
+console.log(str);  // Hello World
