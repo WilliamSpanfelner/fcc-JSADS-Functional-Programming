3aab5b94e4b484ffdb98782e2efdb12266ffc38c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-17 18:44:28 +0300

    Challenge - start

--- 23_someMethodCheck.js
+++ 23_someMethodCheck.js
@@ -9,4 +9,17 @@ const numbers = [10, 50, 8, 220, 110, 11];
 
 console.log(numbers.some(function (currentValue) {
     return currentValue < 10;
-}));
+}));  // true
+
+/* Use the some method inside the checkPositive function to 
+check if any element in arr is positive. The function should 
+return a Boolean value. */
+
+function checkPositive(arr) {
+    // Only change code below this line
+  
+  
+    // Only change code above this line
+  }
+  
+  checkPositive([1, 2, 3, -4, 5]);
